dd9a99ab8e6b3cd68c4cda7e02821d86d8f77fcf
Krushn Dayshmookh <krushn.dayshmookh.ext@siemens.com>
2024-05-03 10:06:45 +0530

    prime react card

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 
 import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext'
+import { FloatLabel } from 'primereact/floatlabel'
+import { Card } from 'primereact/card'
 
 function Auth({ onLogin }) {
   const [username, setUsername] = useState('')
@@ -20,32 +22,37 @@ function Auth({ onLogin }) {
 
   return (
     <>
-      <div>
-        <InputText
-          type="text"
-          placeholder="Username"
-          id="usernameInput"
-          onChange={(e) => {
-            setUsername(e.target.value)
-          }}
-          value={username}
-        />
-        <InputText
-          type="password"
-          placeholder="Password"
-          id="passwordInput"
-          onChange={(e) => {
-            setPassword(e.target.value)
-          }}
-          value={password}
-        />
+      <Card>
+        <FloatLabel>
+          <InputText
+            type="text"
+            id="usernameInput"
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }}
+            value={username}
+          />
+          <label htmlFor="usernameInput">Username</label>
+        </FloatLabel>
+        <FloatLabel>
+          <InputText
+            type="password"
+            placeholder="Password"
+            id="passwordInput"
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            value={password}
+          />
+          <label htmlFor="passwordInput">Password</label>
+        </FloatLabel>
         <Button
           onClick={() => {
             handleLogin()
           }}>
           Login
         </Button>
-      </div>
+      </Card>
     </>
   )
 }
